0b1358f1ede471628d14987a538fd63340785d67
Rokas <rokas.stanislovas.13@ucl.ac.uk>
2016-01-27 18:16:41 +0000

    learning to use matplotlib

--- NeuroML2/Synchronization_analysis.py
+++ NeuroML2/Synchronization_analysis.py
@@ -36,12 +36,8 @@ def Synchronization_analysis(sim_duration,specify_targets,no_of_groups,exp_speci
                    spikes = np.loadtxt('%s/Golgi_pop%d_cell%d.txt'%(sim_dir,pop, cell))
                    spike_train=pyspike.SpikeTrain(spikes,[0,sim_duration])
                    spike_trains.append(spike_train)
-               if trial==0:
-                  ax[0].scatter(spikes,
-                          np.zeros_like(spikes)+cell,
-                          marker='|',
-                          s=2,
-                          c=color)
+                   if trial==0:
+                      ax[0].scatter(spikes,np.zeros_like(spikes)+cell,marker='|',s=2,c=color)
            distances.append(pyspike.spike_profile_multi(spike_trains))
 
     # average synchrony index across trials
@@ -54,7 +50,7 @@ def Synchronization_analysis(sim_duration,specify_targets,no_of_groups,exp_speci
     mark_step=sim_duration/50
     marks=[]
     for mark in range(0,mark_step+1):
-        Mark=mark*mark_step
+        Mark=50*mark
         marks.append(Mark)
 
 
@@ -62,23 +58,32 @@ def Synchronization_analysis(sim_duration,specify_targets,no_of_groups,exp_speci
     xmin = 0
     xmax = sim_duration
     x, y = average_distance.get_plottable_data()
-    ax[1].plot(x,y)
-    #ximin = np.searchsorted(x, xmin)
-    #ximax = np.searchsorted(x, xmax)
-    #lines.append(ax[1].plot(x[ximin:ximax+1], 1-y[ximin:ximax+1], lw=2, c=color)[0])
+    ximin = np.searchsorted(x, xmin)
+    ximax = np.searchsorted(x, xmax)
+    lines.append(ax[1].plot(x[ximin:ximax+1], 1-y[ximin:ximax+1], lw=2, c=color)[0])
 
     #ax[1].plot(x[:ximin+1], 1-y[:ximin+1], lw=2, c=color, alpha=0.4)
     #ax[1].plot(x[ximax:], 1-y[ximax:], lw=2, c=color, alpha=0.4)
 
-    #ax[0].locator_params(tight=True, nbins=mark_step+1)
-    #ax[1].locator_params(axis='y', tight=True, nbins=mark_step+1)
-    #ax[1].set_xticks(marks,('0','50','100','150','200','250','300','350','400','450'))
-    #ax[0].set_ylabel('Cell number')
-    #ax[1].set_xlabel('Time (ms)')
-    #ax[1].set_ylabel('Synchrony index',size=0.02)
-    #plt.tight_layout()
-    #fig.savefig('simulations/desynchronisation_random_graph.pdf')
-    fig.canvas.draw() 
+    ax[0].locator_params(tight=True, nbins=mark_step+1)
+    ax[1].locator_params(axis='y', tight=True, nbins=mark_step+1)
+    ax[1].set_xticks(marks)
+    ax[0].set_ylabel('Cell number',size=5)
+    
+    for subplot in range(0,2):
+        for tick in ax[subplot].yaxis.get_major_ticks():
+            tick.label.set_fontsize(4) 
+     
+    #for subplot in range(0,2):
+        #for tick in ax[subplot].yaxis.get_major_ticks():
+            #tick.label.set_fontsize(8) 
+       
+                
+    ax[1].set_xlabel('Time (ms)')
+    ax[1].set_ylabel('Synchrony index',size=5)
+    plt.tight_layout()
+    plt.savefig('simulations/desynchronisation_random_graph.pdf')
+    
 
 if __name__=="__main__":
    Synchronization_analysis(450,["all"],1,["V2012multi1_2c_1input",["seed specifier",False],1])
