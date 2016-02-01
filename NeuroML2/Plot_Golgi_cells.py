6126c0000837c43313a256cb0bc1fd5e3443e1bd
Rokas <rokas.stanislovas.13@ucl.ac.uk>
2016-02-01 20:21:58 +0000

    testing

---
+++ NeuroML2/Plot_Golgi_cells.py
@@ -0,0 +1,15 @@
+from methods import *
+
+
+if __name__ == "__main__":
+
+#when using "one population one subplot" each pop has to be specified; if no cells have to be plotted for a specific pop pass empty list [] both in the case of "random fraction" and "explicit lists". For example:
+  #   plot_voltage_traces(1,"V2010multi1_2p4c4c_4inp",0,["one population one subplot","random fraction",[[1.0],[]] ],["seed specifier",True],["save specifier",True,"testing_plot1.jpeg"],False)
+
+   #plot_voltage_traces(2,"V2010multi1_2p4c4c_4inp",0,["one population one subplot","random fraction",[[1.0],[1.0]] ],["seed specifier",True],["save specifier",True,"testing_plot1.jpeg"],False)
+
+   plot_voltage_traces(2,"V2010multi1_2p4c4c_4inp",0,["one population one subplot","explicit lists",[[0,1],[0,1]]],["seed specifier",True],["save specifier",True,"testing_plot2.jpeg"],False)  
+
+  # plot_voltage_traces(2,"V2010multi1_2p4c4c_4inp",0,["pairs",[[0,1] ],"explicit lists",[   [  [0,1],[0,1] ]   ]    ],["seed specifier",True], ["save specifier",True,"testing_plot3.pdf"]   ,False)  
+
+   #plot_voltage_traces(2,"V2010multi1_2p4c4c_4inp",0,["pairs",[[0,1]],"random fraction",[  [1,0.5]    ]   ],["seed specifier",True],["save specifier",True,"testing_plot4.pdf"],True)  
