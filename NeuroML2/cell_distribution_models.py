0d76f108ac965952335b2ec40df62fe579c8167e
Rokas <rokas.stanislovas.13@ucl.ac.uk>
2016-02-23 19:11:07 +0000

    moving models to separate modules

---
+++ NeuroML2/cell_distribution_models.py
@@ -0,0 +1,18 @@
+import neuroml
+from pyneuroml import pynml
+from pyneuroml.lems.LEMSSimulation import LEMSSimulation
+import math
+import neuroml.writers as writers
+from neuroml.utils import validate_neuroml2
+import random
+import numpy as np
+import string
+import collections
+
+from methods_v2 import *
+
+
+
+
+
+
