8749111da366ebcab77bbd7f1c05021b42dde346
Rokas <rokas.stanislovas.13@ucl.ac.uk>
2016-02-24 19:01:08 +0000

    no manipulation on paths for now

--- NeuroML2/Example1_v2.py
+++ NeuroML2/Example1_v2.py
@@ -1,9 +1,4 @@
 
-import os,sys,inspect
-currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
-parentdir = os.path.dirname(currentdir)
-sys.path.insert(0,parentdir) 
-print parentdir
 
 from Simulate_Golgi_Network_v2 import *
 
