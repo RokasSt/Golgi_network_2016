3b790fc37c73f7a987a8f552794cc9c863cf38b9
Rokas <rokas.stanislovas.13@ucl.ac.uk>
2016-02-24 19:11:24 +0000

    reverted

--- NeuroML2/Example1_v2.py
+++ NeuroML2/Example1_v2.py
@@ -1,4 +1,9 @@
 
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+print parentdir
 
 from Simulate_Golgi_Network_v2 import *
 
@@ -23,7 +28,18 @@ if __name__ == "__main__":
     net_params_test_2010_multiple['experiment1']['distributionParams']['yDim']=100
     net_params_test_2010_multiple['experiment1']['distributionParams']['zDim']=100
 
+    ##### below lines to make a reference template for the density based model:
+    #   net_params_test_2010_multiple['experiment1']['distributionParams']={}
+    #   net_params_test_2010_multiple['experiment1']['distributionParams']['distributionModel']="density based"
+    #   net_params_test_2010_multiple['experiment1']['distributionParams']['populationList']=[]
+    #   net_params_test_2010_multiple['experiment1']['distributionParams']['populationList'].append({'popID':'Golgi_pop0','densityFilePath':'/home/rokas/Golgi_data/GlyT2 density matrix of shape 35 152.txt',\
+    #   'planeDimensions':{'dim1':'x','dim2':'z'},'dim1CoordinateVector':[1300,1500],'dim2CoordinateVector':[0,50]})
+    #   net_params_test_2010_multiple['experiment1']['distributionParams']['populationList'].append({'popID':'Golgi_pop1','densityFilePath':'/home/rokas/Golgi_data/GlyT2 density matrix of shape 35 152.txt',\
+    #   'planeDimensions':{'dim1':'x','dim2':'z'},'dim1CoordinateVector':[1300,1500],'dim2CoordinateVector':[0,50],'dim3':'y','dim3CoordinateVector':[0,200],\
+    #   'distanceModel':'minimal_distance','minimalDistance':25})
     
+
+    ##### no need to have explicit subcellular targeting in the case of soma-to-soma distance dependent conductance mode ("Vervaeke_2010_based")
     net_params_test_2010_multiple['experiment1']['connParams']={}
     net_params_test_2010_multiple['experiment1']['connParams']['populationPairs']=[]
     net_params_test_2010_multiple['experiment1']['connParams']['populationPairs'].append({'electricalConnModel':"Vervaeke_2010_based",'prePopID':'Golgi_pop0',\
