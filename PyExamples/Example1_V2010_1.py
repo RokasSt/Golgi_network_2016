9ed87dc5b00736be55f159d1b205da9ec7f69b8c
Rokas <rokas.stanislovas.13@ucl.ac.uk>
2016-03-14 21:11:38 +0000

    further testing running networks

--- PyExamples/Example1_V2010_1.py
+++ PyExamples/Example1_V2010_1.py
@@ -12,18 +12,18 @@ if __name__ == "__main__":
 
     net_params_test_2010_multiple={}
     net_params_test_2010_multiple['experiment1']={}
-    net_params_test_2010_multiple['experiment2']={}
+    #net_params_test_2010_multiple['experiment2']={}
     
-    net_params_test_2010_multiple['experiment1']['experimentID']="test_V2010_exp1"
-    net_params_test_2010_multiple['experiment2']['experimentID']="test_V2010_exp2"
+    net_params_test_2010_multiple['experiment1']['experimentID']="test_V2010_exp1_10cells"
+    #net_params_test_2010_multiple['experiment2']['experimentID']="test_V2010_exp2"
     #exp1 note that the number of cells can easily be given based on the uniform density (previous studies around 4607 cells/ per mm3) and x,y,z dimensions in micro m.
     net_params_test_2010_multiple['experiment1']['popParams']=[]
-    net_params_test_2010_multiple['experiment1']['popParams'].append({'popID':'Golgi_pop0','cellType':"Golgi_5comp_3channels_1CaPool",'size':180})
+    net_params_test_2010_multiple['experiment1']['popParams'].append({'popID':'Golgi_pop0','cellType':"Golgi_5comp_3channels_1CaPool",'size':10})
    
 
     net_params_test_2010_multiple['experiment1']['distributionParams']={}
     net_params_test_2010_multiple['experiment1']['distributionParams']['populationList']=[]
-    net_params_test_2010_multiple['experiment1']['distributionParams']['populationList'].append({'popID':'Golgi_pop0','distributionModel':'explicit_cell_numbers','distanceModel':'random_minimal_distance','minimal_distance':30,'xDim':700,'yDim':700,'zDim':80})
+    net_params_test_2010_multiple['experiment1']['distributionParams']['populationList'].append({'popID':'Golgi_pop0','distributionModel':'explicit_cell_numbers','distanceModel':'random_minimal_distance','minimal_distance':30,'xDim':80,'yDim':80,'zDim':80})
    
     
 
@@ -54,37 +54,37 @@ if __name__ == "__main__":
 
 
     #exp2
-    net_params_test_2010_multiple['experiment2']['popParams']=[]
-    net_params_test_2010_multiple['experiment2']['popParams'].append({'popID':'Golgi_pop0','cellType':"Golgi_5comp_3channels_1CaPool",'size':180})
+    #net_params_test_2010_multiple['experiment2']['popParams']=[]
+    #net_params_test_2010_multiple['experiment2']['popParams'].append({'popID':'Golgi_pop0','cellType':"Golgi_5comp_3channels_1CaPool",'size':180})
    
 
-    net_params_test_2010_multiple['experiment2']['distributionParams']={}
-    net_params_test_2010_multiple['experiment2']['distributionParams']['populationList']=[]
-    net_params_test_2010_multiple['experiment2']['distributionParams']['populationList'].append({'popID':'Golgi_pop0','distributionModel':'explicit_cell_numbers','distanceModel':'random_no_overlap','xDim':700,'yDim':700,'zDim':80})
+    #net_params_test_2010_multiple['experiment2']['distributionParams']={}
+    #net_params_test_2010_multiple['experiment2']['distributionParams']['populationList']=[]
+    #net_params_test_2010_multiple['experiment2']['distributionParams']['populationList'].append({'popID':'Golgi_pop0','distributionModel':'explicit_cell_numbers','distanceModel':'random_no_overlap','xDim':700,'yDim':700,'zDim':80})
    
 
     ##### as conductance levels are soma-to-soma distance-dependent in a Vervaeke_2010_based model, subcellular targeting is not implemented in this configuration. If   one wants to specify multiple GJs per cell pair with constant or heterogeneous conductances one can use Vervaeke_2012_based connModel
-    net_params_test_2010_multiple['experiment2']['connParams']={}
-    net_params_test_2010_multiple['experiment2']['connParams']['populationPairs']=[]
-    net_params_test_2010_multiple['experiment2']['connParams']['populationPairs'].append({'electricalConnModel':"Vervaeke_2010_based",'prePopID':'Golgi_pop0',\
-                                                    'postPopID':'Golgi_pop0','spatialScale':20,'testingConductanceScale':1,'units':'nS','normalizeConductances':True,\
-                            'prePoptargetGroup':{'segmentGroupList':["dendrite_group"],'segmentGroupProbabilities':{"dendrite_group":1}},\
-                            'postPoptargetGroup':{'segmentGroupList':["dendrite_group"],'segmentGroupProbabilities':{"dendrite_group":1}}})
+    #net_params_test_2010_multiple['experiment2']['connParams']={}
+    #net_params_test_2010_multiple['experiment2']['connParams']['populationPairs']=[]
+    #net_params_test_2010_multiple['experiment2']['connParams']['populationPairs'].append({'electricalConnModel':"Vervaeke_2010_based",'prePopID':'Golgi_pop0',\
+                                               #     'postPopID':'Golgi_pop0','spatialScale':20,'testingConductanceScale':1,'units':'nS','normalizeConductances':True,\
+                          #  'prePoptargetGroup':{'segmentGroupList':["dendrite_group"],'segmentGroupProbabilities':{"dendrite_group":1}},\
+                           # 'postPoptargetGroup':{'segmentGroupList':["dendrite_group"],'segmentGroupProbabilities':{"dendrite_group":1}}})
 
-    net_params_test_2010_multiple['experiment2']['inputParams']=[]
-    inputGroups_pop0_2010exp2=[]
-    inputGroups_pop0_2010exp2.append({'inputModel':"testing",'inputLabel':'PulsePop0','testingModel':"pulseGenerators",'cellFractionToTarget':0.5,\
-                    'pulseParameters':[{'delay':20,'duration':200,'amplitude':4E-5}],\
-                         'ampUnits':"uA",'timeUnits':'ms'})
+    #net_params_test_2010_multiple['experiment2']['inputParams']=[]
+    #inputGroups_pop0_2010exp2=[]
+    #inputGroups_pop0_2010exp2.append({'inputModel':"testing",'inputLabel':'PulsePop0','testingModel':"pulseGenerators",'cellFractionToTarget':0.5,\
+                  #  'pulseParameters':[{'delay':20,'duration':200,'amplitude':4E-5}],\
+                      #   'ampUnits':"uA",'timeUnits':'ms'})
 
    
 
-    net_params_test_2010_multiple['experiment2']['inputParams'].append({'popName':'Golgi_pop0','inputGroups':inputGroups_pop0_2010exp2})
+    #net_params_test_2010_multiple['experiment2']['inputParams'].append({'popName':'Golgi_pop0','inputGroups':inputGroups_pop0_2010exp2})
    
 
     ####### change 'simulator' to "jNeuroML_NEURON" in order to run simulations in NEURON
 
-    sim_params={'simulator':"jNeuroML_NEURON",'duration':450,'timeStep':0.005,'numTrials':5,'globalSeed':False,'trialSeed':True,'plotSpecifier':False,\
+    sim_params={'simulator':"jNeuroML_NEURON",'duration':3000,'timeStep':0.005,'numTrials':1,'globalSeed':False,'trialSeed':True,'plotSpecifier':False,\
     'saveSomataPositions':True,'parentDirRequired':True,'parentDir':parentdir,'currentDirRequired':True,'currentDir':currentdir,'networkDir':'example',\
     'saveInputReceivingCellID':True}
     
--- PyExperiments/V2010_test_savings.py
+++ PyExperiments/V2010_test_savings.py
@@ -80,8 +80,8 @@ if __name__ == "__main__":
     
     #######
     
-    library_params={'libraryScale':1,'simulator':'jNeuroML_NEURON','timeStep':0.01}
-    sim_params={'simulator':"no simulation",'duration':2000,'timeStep':0.0003,'numTrials':1,'globalSeed':False,'trialSeed':True,'plotSpecifier':False,\
+    library_params={'libraryScale':1,'simulator':'jNeuroML_NEURON','timeStep':0.005}
+    sim_params={'simulator':"no simulation",'duration':450,'timeStep':0.0003,'numTrials':1,'globalSeed':False,'trialSeed':True,'plotSpecifier':False,\
     'saveSomataPositions':True,'parentDirRequired':True,'parentDir':parentdir,'currentDirRequired':True,'currentDir':currentdir,'networkDir':'example',\
         'saveInputReceivingCellID':True,'importPoissonTrainLibraries':True,'PoissonTrainLibraryID':'newlyGenerated','libraryParams':library_params}
     
